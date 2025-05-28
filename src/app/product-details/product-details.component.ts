93372b975ecba1fd9eb5e9ac5abe69ea8e608d0e
Nathi The Creator <nts2melo@gmail.com>
2025-05-28 15:15:25 +0200

    added a buy feature in the product details page

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { MenuService } from '../services/menu.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, ActivatedRoute } from '@angular/router';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -13,6 +14,7 @@ import { RouterModule, ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit {
   menuService = inject(MenuService);
+  cartService = inject(CartService);
   route = inject(ActivatedRoute);
   product: any = null;
   errorMessage: string | null = null;
@@ -33,6 +35,19 @@ export class ProductDetailsComponent implements OnInit {
     } else {
       this.errorMessage = 'No product ID provided';
     }
+  }
+
+   addToCart(menuItem: any): void {
+    this.cartService.addItem(menuItem).subscribe({
+      next: (cartItem) => {
+        console.log('Item added to cart:', cartItem);
+      
+      },
+      error: (err) => {
+        console.error('Failed to add item to cart:', err);
+        // Optionally, show an error message to the user
+      }
+    });
   }
 
   increaseQuantity() {
