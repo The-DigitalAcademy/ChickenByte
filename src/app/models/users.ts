9ee8450b47bc2be1b48ffb6929142b1ebe562c4e
maleke2001 <motshabielizabeth1@gmail.com>
2025-06-15 20:36:58 +0200

    extend User interface with Address and add new fields

--- src/app/models/users.ts
+++ src/app/models/users.ts
@@ -1,6 +1,11 @@
-export interface User {
+import { Address } from "./address";
+
+export interface User extends Address {
+    id?: number;
     name: string;
     email: string;
     password: string;
+    phone?: string; // Changed from number to string for better phone number handling
 }
+
 
